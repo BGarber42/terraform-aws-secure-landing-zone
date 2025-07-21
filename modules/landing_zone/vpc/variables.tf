4209074b95c2419827cfa09abb9a182497639d31
Brent Garber <me@bgarber.work>
2025-07-21 10:37:42 -0400

    Add VPC module with public/private subnets and networking components

---
+++ modules/landing_zone/vpc/variables.tf
@@ -0,0 +1,38 @@
+variable "account_id" {
+  description = "AWS Account ID"
+  type        = string
+}
+
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default = {
+    Terraform   = "true"
+    Environment = "prod"
+    Owner       = "platform"
+  }
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "CIDR blocks for public subnets"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "private_subnet_cidrs" {
+  description = "CIDR blocks for private subnets"
+  type        = list(string)
+  default     = ["10.0.11.0/24", "10.0.12.0/24"]
+} 
